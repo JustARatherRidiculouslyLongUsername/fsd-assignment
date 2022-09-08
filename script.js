5614ebd1e91857c6324b98efc6f4baece55bf947
Viswas Haridas <viswas.b10@gmail.com>
2022-09-08 12:22:10 +0530

    Assign unique IDs to each checkbox for `<label>`s to work

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const input = document.querySelector('.form-control');
+let items = 1;
 
 input.addEventListener('keypress', e => {
     if (e.key !== 'Enter') return;
 
     const copy = document.querySelector('.form-check').cloneNode(true);
+    const id = `checkbox-${items++}`
+    copy.children[0].id = id;
+    copy.children[1].setAttribute('for', id);
     copy.children[1].innerHTML = document.querySelector('.form-control').value;
 
     document.querySelector('main').append(copy);
